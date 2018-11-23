096f41a32c32a5191d528a45ebf78b7dcebe80d1
Valentin Py <valentin.py@he-arc.ch>
2018-11-23 18:08:11 +0100

    start of kaist porting for eval. do_python_eval_kaist() not done yet

--- eval.py
+++ eval.py
@@ -9,9 +9,19 @@ import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
-from data import VOC_ROOT, VOCAnnotationTransform, VOCDetection, BaseTransform
-from data import VOC_CLASSES as labelmap
+from data import BaseTransform
+
+from data import VOC_ROOT, VOCAnnotationTransform, VOCDetection
+from data import VOC_CLASSES as VOClabelmap
+
+from data import KAIST_ROOT, KAISTAnnotationTransform, KAISTDetection
+from data import KAIST_CLASSES as KAISTlabelmap
+
 import torch.utils.data as data
+from utils.timer import Timer
+
+#TODO VPY import kaist
+#TODO VPY developp annotation parser
 
 from ssd import build_ssd
 
@@ -22,74 +32,33 @@ import argparse
 import numpy as np
 import pickle
 import cv2
-
-if sys.version_info[0] == 2:
-    import xml.etree.cElementTree as ET
-else:
-    import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
 
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
+def arg_parser():
 
-parser = argparse.ArgumentParser(
-    description='Single Shot MultiBox Detector Evaluation')
-parser.add_argument('--trained_model',
-                    default='weights/ssd300_mAP_77.43_v2.pth', type=str,
-                    help='Trained state_dict file path to open')
-parser.add_argument('--confidence_threshold', default=0.01, type=float,
-                    help='Detection confidence threshold')
-parser.add_argument('--cuda', default=True, type=str2bool,
-                    help='Use cuda to train model')
-parser.add_argument('--voc_root', default=VOC_ROOT,
-                    help='Location of VOC root directory')
+    parser = argparse.ArgumentParser(
+        description='Single Shot MultiBox Detector Evaluation')
+    parser.add_argument('--dataset_type', default='VOC', choices=['VOC', 'COCO', 'KAIST'],
+                        type=str, help='VOC, COCO or KAIST (requires image_set)')
+    parser.add_argument('--image_set', default=None,
+                        help='[KAIST] Imageset')
+    parser.add_argument('--trained_model',
+                        default='weights/ssd300_mAP_77.43_v2.pth', type=str,
+                        help='Trained state_dict file path to open')
+    parser.add_argument('--confidence_threshold', default=0.01, type=float,
+                        help='Detection confidence threshold')
+    parser.add_argument('--cuda', default=True, type=str2bool,
+                        help='Use cuda to train model')
+    parser.add_argument('--dataset_root', default=VOC_ROOT,
+                        help='Location of dataset root directory')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if torch.cuda.is_available():
-    if args.cuda:
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    if not args.cuda:
-        print("WARNING: It looks like you have a CUDA device, but aren't using \
-              CUDA.  Run with --cuda for optimal eval speed.")
-        torch.set_default_tensor_type('torch.FloatTensor')
-else:
-    torch.set_default_tensor_type('torch.FloatTensor')
-
-annopath = os.path.join(args.voc_root, 'VOC2007', 'Annotations', '%s.xml')
-imgpath = os.path.join(args.voc_root, 'VOC2007', 'JPEGImages', '%s.jpg')
-imgsetpath = os.path.join(args.voc_root, 'VOC2007', 'ImageSets',
-                          'Main', '{:s}.txt')
-YEAR = '2007'
-devkit_path = args.voc_root + 'VOC' + YEAR
-dataset_mean = (104, 117, 123)
-set_type = 'test'
-
-
-class Timer(object):
-    """A simple timer."""
-    def __init__(self):
-        self.total_time = 0.
-        self.calls = 0
-        self.start_time = 0.
-        self.diff = 0.
-        self.average_time = 0.
-
-    def tic(self):
-        # using time.time instead of time.clock because time time.clock
-        # does not normalize for multithreading
-        self.start_time = time.time()
-
-    def toc(self, average=True):
-        self.diff = time.time() - self.start_time
-        self.total_time += self.diff
-        self.calls += 1
-        self.average_time = self.total_time / self.calls
-        if average:
-            return self.average_time
-        else:
-            return self.diff
+    return args
 
 
 def parse_rec(filename):
@@ -133,6 +102,15 @@ def get_voc_results_file_template(image_set):
     path = os.path.join(filedir, filename)
     return path
 
+def get_kaist_results_file_template(image_set):
+    # VOCdevkit/VOC2007/results/det_test_aeroplane.txt
+    filename = 'det_' + image_set + '_%s.txt'
+    filedir = os.path.join(args.dataset_root, 'results')
+    if not os.path.exists(filedir):
+        os.makedirs(filedir)
+    path = os.path.join(filedir, filename)
+    return path
+
 
 def write_voc_results_file(all_boxes, dataset):
     for cls_ind, cls in enumerate(labelmap):
@@ -150,8 +128,24 @@ def write_voc_results_file(all_boxes, dataset):
                                    dets[k, 0] + 1, dets[k, 1] + 1,
                                    dets[k, 2] + 1, dets[k, 3] + 1))
 
+def write_kaist_results_file(all_boxes, dataset):
+    for cls_ind, cls in enumerate(labelmap):
+        print('Writing {:s} KAIST results file'.format(cls))
+        filename = get_kaist_results_file_template(set_type) % (cls)
+        with open(filename, 'wt') as f:
+            for im_ind, index in enumerate(dataset.ids):
+                dets = all_boxes[cls_ind+1][im_ind]
+                if dets == []:
+                    continue
+                # the VOCdevkit expects 1-based indices
+                for k in range(dets.shape[0]):
+                    f.write('{:s}/{:s}/{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                            format(index[1], index[2], index[3], dets[k, -1],
+                                   dets[k, 0] + 1, dets[k, 1] + 1,
+                                   dets[k, 2] + 1, dets[k, 3] + 1))
 
-def do_python_eval(output_dir='output', use_07=True):
+
+def do_python_eval_voc(output_dir='output', use_07=True):
     cachedir = os.path.join(devkit_path, 'annotations_cache')
     aps = []
     # The PASCAL VOC metric changed in 2010
@@ -180,6 +174,34 @@ def do_python_eval(output_dir='output', use_07=True):
     print('Results computed with the **unofficial** Python eval code.')
     print('Results should be very close to the official MATLAB eval code.')
     print('--------------------------------------------------------------')
+
+
+
+def do_python_eval_kaist(output_dir='output', use_07=True):
+    cachedir = os.path.join(args.dataset_root, 'annotations_cache')
+    aps = []
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    for i, cls in enumerate(labelmap):
+        filepath = get_kaist_results_file_template(set_type)
+        rec, prec, ap = kaist_eval(filepath, annopath, imgsetpath.format(set_type), cls, cachedir, ovthresh=0.5, use_07_metric=use_07_metric)
+        aps += [ap]
+        print('AP for {} = {:.4f}'.format(cls, ap))
+        with open(os.path.join(output_dir, cls + '_pr.pkl'), 'wb') as f:
+            pickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
+    print('Mean AP = {:.4f}'.format(np.mean(aps)))
+    print('~~~~~~~~')
+    print('Results:')
+    for ap in aps:
+        print('{:.3f}'.format(ap))
+    print('{:.3f}'.format(np.mean(aps)))
+    print('~~~~~~~~')
+    print('')
+    print('--------------------------------------------------------------')
+    print('Results computed with the **unofficial** Python eval code.')
+    print('Results should be very close to the official MATLAB eval code.')
+    print('--------------------------------------------------------------')
+
 
 
 def voc_ap(rec, prec, use_07_metric=True):
@@ -245,6 +267,7 @@ cachedir: Directory for caching the annotations
 # assumes annotations are in annopath.format(imagename)
 # assumes imagesetfile is a text file with each line an image name
 # cachedir caches the annotations in a pickle file
+
 # first load gt
     if not os.path.isdir(cachedir):
         os.mkdir(cachedir)
@@ -352,8 +375,7 @@ cachedir: Directory for caching the annotations
     return rec, prec, ap
 
 
-def test_net(net, cuda, dataset, transform,
-             im_size=300, thresh=0.05):
+def test_net(net, cuda, dataset, transform, im_size=300, thresh=0.05, dataset_type=None):
     num_images = len(dataset)
     # all detections are collected into:
     #    all_boxes[cls][image] = N x 5 array of detections in
@@ -363,10 +385,17 @@ def test_net(net, cuda, dataset, transform,
 
     # timers
     _t = {'im_detect': Timer(), 'misc': Timer()}
-    output_dir = get_output_dir('ssd300_120000', set_type)
+
+    # create eval result folder
+    output_dir = get_output_dir(dataset.name + '_eval_ssd300_120000', set_type)
     det_file = os.path.join(output_dir, 'detections.pkl')
 
-    for i in range(num_images):
+    # loop for all test images
+    # for i in range(num_images): #TODO VPY debug
+    for i in range(10):  # TODO VPY debug
+        print("!! Debug mode!! not all images are tested")
+
+        # get image + annotations + dimensions
         im, gt, h, w = dataset.pull_item(i)
 
         x = Variable(im.unsqueeze(0))
@@ -401,29 +430,83 @@ def test_net(net, cuda, dataset, transform,
         pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
 
     print('Evaluating detections')
-    evaluate_detections(all_boxes, output_dir, dataset)
+    evaluate_detections(all_boxes, output_dir, dataset, dataset_type)
 
 
-def evaluate_detections(box_list, output_dir, dataset):
-    write_voc_results_file(box_list, dataset)
-    do_python_eval(output_dir)
+def evaluate_detections(box_list, output_dir, dataset, dataset_type):
+    if dataset_type == "VOC":
+        write_voc_results_file(box_list, dataset)
+        do_python_eval_voc(output_dir)
+    elif dataset_type == "KAIST":
+        write_kaist_results_file(box_list, dataset)
+        do_python_eval_kaist(output_dir)
+        # raise NotImplementedError
+        #TODO VPY implement
+    else:
+        print("Dataset not supported")
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
+
+    # parse arguments
+    args = arg_parser()
+
+    # prepare environnement
+    if torch.cuda.is_available():
+        if args.cuda:
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        if not args.cuda:
+            print("WARNING: It looks like you have a CUDA device, but aren't using \
+                  CUDA.  Run with --cuda for optimal eval speed.")
+            torch.set_default_tensor_type('torch.FloatTensor')
+    else:
+        torch.set_default_tensor_type('torch.FloatTensor')
+
+    if args.dataset_type == "VOC":
+        annopath = os.path.join(args.dataset_root, 'VOC2007', 'Annotations', '%s.xml')
+        imgpath = os.path.join(args.dataset_root, 'VOC2007', 'JPEGImages', '%s.jpg')
+        imgsetpath = os.path.join(args.dataset_root, 'VOC2007', 'ImageSets', 'Main', '{:s}.txt')
+        YEAR = '2007'
+        devkit_path = args.dataset_root + 'VOC' + YEAR
+        labelmap = VOClabelmap
+
+    elif args.dataset_type == "KAIST":
+        annopath = os.path.join(args.dataset_root, 'rgbt-ped-detection/data/kaist-rgbt/', '%s', 'annotations', '%s', '%s', '%s.txt')
+        imgpath = os.path.join(args.dataset_root, 'rgbt-ped-detection/data/kaist-rgbt/', '%s', 'images', '%s', '%s', 'visible', '%s.jpg')
+        imgsetpath = os.path.join(args.dataset_root, 'rgbt-ped-detection/data/kaist-rgbt/', '%s', 'imageSets', '%s.jpg')
+        labelmap = KAISTlabelmap
+        # img_lwir_root_path = os.path.join(args.dataset_root, 'rgbt-ped-detection/data/kaist-rgbt/', '%s', 'images', '%s', '%s', 'lwir', '%s.jpg')
+        #YEAR = None
+        #devkit_path = None
+        #raise NotImplementedError
+    else:
+        print("Dataset not implemented")
+        raise NotImplementedError
+
+    dataset_mean = (104, 117, 123)  # TODO VPY and for kaist ?
+    set_type = 'test'
+
     # load net
     num_classes = len(labelmap) + 1                      # +1 for background
-    net = build_ssd('test', 300, num_classes)            # initialize SSD
+    net = build_ssd('test', 300, num_classes, args.dataset_type)            # initialize SSD
     net.load_state_dict(torch.load(args.trained_model))
     net.eval()
     print('Finished loading model!')
     # load data
-    dataset = VOCDetection(args.voc_root, [('2007', set_type)],
-                           BaseTransform(300, dataset_mean),
-                           VOCAnnotationTransform())
+
+    if args.dataset_type == "VOC":
+        dataset = VOCDetection(root=args.dataset_root, image_sets=[('2007', set_type)], transform=BaseTransform(300, dataset_mean), target_transform=VOCAnnotationTransform())
+    elif args.dataset_type == "KAIST":
+        dataset = KAISTDetection(root=args.dataset_root,image_set=args.image_set, transform=BaseTransform(300, dataset_mean), target_transform=KAISTAnnotationTransform(), dataset_name="KAIST")
+    else:
+        print("dataset not supported: {}".format(args.dataset_type))
+        sys.exit(-1)
+
     if args.cuda:
         net = net.cuda()
         cudnn.benchmark = True
     # evaluation
-    test_net(net, args.cuda, dataset,
-             BaseTransform(net.size, dataset_mean), 300,
-             thresh=args.confidence_threshold)
+    test_net(net=net, cuda=args.cuda, dataset=dataset,
+             transform=BaseTransform(net.size, dataset_mean), im_size=300,
+             thresh=args.confidence_threshold, dataset_type=args.dataset_type)
