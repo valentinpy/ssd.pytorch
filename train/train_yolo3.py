d8c3cb9146956cd7fecb39328167fe82b8758355
Valentin Py <valentin.py@he-arc.ch>
2018-12-19 13:32:10 +0100

    train yolo for new parameters + add visualization

--- train/train_yolo3.py
+++ train/train_yolo3.py
@@ -3,85 +3,71 @@ from __future__ import division
 from models import *
 # from utils.utils import *
 # from utils.coco import *
-
 import os
 import sys
 import time
 import datetime
 import argparse
-
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
-
 from utils.str2bool import str2bool
-
 from config.load_classes import load_classes
-
 from config.parse_config import *
 from models.yolo3 import weights_init_normal
 from models.yolo3 import Darknet
-
 import numpy as np
+from utils.visualization import vis_plot
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='YOLO3 Detector Training With Pytorch')
-    # train_set = parser.add_mutually_exclusive_group()
-    parser.add_argument('--dataset_type', default='VOC', choices=['VOC', 'COCO', 'KAIST'],type=str, help='VOC, COCO or KAIST (requires image_set )')
-    parser.add_argument('--dataset_root', default=None, help='Dataset root directory path') #TODO VPY: should be in kaist.data
     parser.add_argument('--image_set', default=None, help='[KAIST] Imageset')
-    parser.add_argument('--basenet', default=None, help='Pretrained base model') #TODO VPY: should be in kaist.data
-    parser.add_argument('--batch_size', default=4, type=int, help='Batch size for training') #TODO VPY: should be in kaist.data
     parser.add_argument('--resume', default=None, type=str, help='Checkpoint state_dict file to resume training from')
     parser.add_argument('--start_iter', default=0, type=int, help='Resume training at this iter')
-    parser.add_argument('--save_frequency', default=5000, type=int, help='Frequency to save model [default: 5000 iters]')
-    parser.add_argument('--num_workers', default=0, type=int, help='Number of workers used in dataloading') #TODO 4
-    parser.add_argument('--cuda', default=True, type=str2bool, help='Use CUDA to train model')
-    # parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
-    # parser.add_argument('--momentum', default=0.9, type=float, help='Momentum value for optim')
-    # parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
-    # parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
-    parser.add_argument('--visdom', default=False, type=str2bool, help='Use visdom for loss visualization')
     parser.add_argument('--save_folder', default='checkpoints', help='Directory for saving checkpoint models')
     parser.add_argument('--image_fusion', default=-1, type=int, help='[KAIST]: type of image fusion: [0: visible], [1: lwir] [2: lwir inverted][...]') #TODO VPY update when required
-    # parser.add_argument('--show_dataset', default=False, type=str2bool, help='Show every image used ?')
-    parser.add_argument("--model_config_path", type=str, default="config/yolov3.cfg", help="path to model config file")
-    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
-    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-    parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
+    parser.add_argument('--show_dataset', default=False, type=str2bool, help='Show every image used ?')
     parser.add_argument("--data_config_path", type=str, default=None, help="path to data config file")
     args = parser.parse_args()
     return args
 
 
 def main(args):
-    cuda = torch.cuda.is_available() and args.cuda
+    args = vars(arg_parser())
+    config = parse_data_config(args['data_config_path'])
+    args = {**args, **config}
+    del config
+
+    if args['visdom']:
+        viz = vis_plot()
+    else:
+        viz = None
+
+    cuda = torch.cuda.is_available() and args['cuda']
 
     os.makedirs("output", exist_ok=True)
     os.makedirs("checkpoints", exist_ok=True)
 
-    classes = load_classes(args.class_path)
+    classes = load_classes(args['names'])
 
     # Get data configuration
-    data_config = parse_data_config(args.data_config_path)
-    train_path = data_config["train"]
-    iters = int(data_config["iters"])
+    # data_config = parse_data_config(args.data_config_path)
+    iters = args['yolo_max_iters']
 
     # Get hyper parameters
-    hyperparams = parse_model_config(args.model_config_path)[0]
+    hyperparams = parse_model_config(args['yolo_model_config_path'])[0]
     learning_rate = float(hyperparams["learning_rate"])
     momentum = float(hyperparams["momentum"])
     decay = float(hyperparams["decay"])
     burn_in = int(hyperparams["burn_in"])
-    model = Darknet(args.model_config_path)
+    model = Darknet(args['yolo_model_config_path'])
 
     # Initiate model
-    if args.basenet is not None:
-        model.load_weights(args.basenet)
+    if args['yolo_initial_weights'] is not None:
+        model.load_weights(args['yolo_initial_weights'])
     else:
         model.apply(weights_init_normal)
 
@@ -91,50 +77,60 @@ def main(args):
     model.train()
 
     # Get dataloader
-    if args.dataset_type == "COCO":
-        from data.coco_list import ListDataset
+    if args['name'] == "COCO":
+        from data.coco_list import ListDataset, detection_collate_COCO_YOLO
+        train_path = args["train_set"]
         dataset = ListDataset(train_path)
         dataloader = torch.utils.data.DataLoader(
             dataset=dataset,
-            batch_size=args.batch_size,
+            batch_size=int(hyperparams['batch']),
             shuffle=False,
-            num_workers=args.num_workers)
-    elif args.dataset_type == "KAIST":
-        from data.kaist import KAISTDetection, KAISTAnnotationTransform
+            num_workers=args['num_workers'],
+            collate_fn=detection_collate_COCO_YOLO)
+    elif args['name'] == "KAIST":
+        from data.kaist import KAISTDetection, KAISTAnnotationTransform, detection_collate_KAIST_YOLO
         from augmentations.YOLOaugmentations import YOLOaugmentation
         # dataset = KAISTDetection(root=data_config["dataset_root"], image_set=data_config["train"], transform=SSDAugmentation(args.img_size), image_fusion=int(data_config["image_fusion"]))
 
-        kaist_root = data_config["dataset_root"]
-        image_set = data_config["train"]
-        image_fusion = int(data_config["image_fusion"])
+        kaist_root = args["dataset_root"]
+        image_set = args["image_set"]
+        image_fusion = args["image_fusion"]
 
-        dataset = KAISTDetection(root=kaist_root, image_set=image_set, transform=YOLOaugmentation(args.img_size), image_fusion=image_fusion, output_format="YOLO", target_transform=KAISTAnnotationTransform(output_format="YOLO"))
+        dataset = KAISTDetection(root=kaist_root, image_set=image_set, transform=YOLOaugmentation(args['yolo_img_size']), image_fusion=image_fusion, output_format="YOLO", target_transform=KAISTAnnotationTransform(output_format="YOLO"))
         dataloader = torch.utils.data.DataLoader(
             dataset=dataset,
-            batch_size=args.batch_size,
+            batch_size=int(hyperparams['batch']),
             shuffle=False, #True,
-            num_workers=args.num_workers
+            num_workers=args['num_workers'],
+            collate_fn=detection_collate_KAIST_YOLO
             # collate_fn=torch.utils.data.dataloader.default_collate,
             # pin_memory=True
             )
-        print("VPY: we might want to change collate_fn ?") #TODO VPY
     else:
         raise NotImplementedError
+
+    if args['visdom']:
+        vis_title = 'YOLO3.PyTorch on ' + dataset.name + ' | lr: ' + "n/d"
+        vis_legend = ['Model loss', 'n/d', 'Total Loss']
+        iter_plot = viz.create_vis_plot('Iteration', 'Loss', vis_title, vis_legend)
+        # epoch_plot = viz.create_vis_plot('Epoch', 'Loss', vis_title, vis_legend)
+
 
     Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
 
+    iteration_total = 0
     for epoch in range(iters):
-        for batch_i, (_, imgs, targets, _, _) in enumerate(dataloader):
+        for batch_i, (imgs, targets) in enumerate(dataloader):
             #print("In learning loop:\nbatch_i: {}\ntmp: {}\n imgs.shape: {}\n targets.shape: {}\n\n".format(batch_i, tmp, imgs.shape, targets.shape))
             imgs = Variable(imgs.type(Tensor))
 
             import cv2
             test_img = np.transpose(imgs[0].cpu().numpy(), (1, 2, 0))
             test_img = test_img[:,:,(2,1,0)]
-            cv2.imshow('image', test_img)
-            cv2.waitKey(500)
+            # cv2.imshow('image', test_img)
+            # cv2.waitKey(500)
 
             targets = Variable(targets.type(Tensor), requires_grad=False)
 
@@ -145,29 +141,39 @@ def main(args):
             loss.backward()
             optimizer.step()
 
-            print(
-                "[Epoch %d/%d, Batch %d/%d] [Losses: x %f, y %f, w %f, h %f, conf %f, cls %f, total %f, recall: %.5f, precision: %.5f]"
-                % (
-                    epoch,
-                    iters,
-                    batch_i,
-                    len(dataloader),
-                    model.losses["x"],
-                    model.losses["y"],
-                    model.losses["w"],
-                    model.losses["h"],
-                    model.losses["conf"],
-                    model.losses["cls"],
-                    loss.item(),
-                    model.losses["recall"],
-                    model.losses["precision"],
+            if batch_i % 100 == 0:
+                print(
+                    "[%s] [Epoch %d/%d, Batch %d/%d, total %d/%d] [Losses: x %f, y %f, w %f, h %f, conf %f, cls %f, total %f, recall: %.5f, precision: %.5f]"
+                    % (
+                        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                        epoch,
+                        iters,
+                        batch_i,
+                        len(dataloader),
+                        epoch * len(dataloader) + batch_i,
+                        iters*len(dataloader),
+                        model.losses["x"],
+                        model.losses["y"],
+                        model.losses["w"],
+                        model.losses["h"],
+                        model.losses["conf"],
+                        model.losses["cls"],
+                        loss.item(),
+                        model.losses["recall"],
+                        model.losses["precision"],
+                    )
                 )
-            )
 
             model.seen += imgs.size(0)
 
-        if epoch % args.checkpoint_interval == 0:
-            model.save_weights("%s/%d.weights" % (args.checkpoint_dir, epoch))
+            if (((epoch*len(dataloader))+ batch_i) % (args['save_frequency']) == 0 ) and (epoch*len(dataloader)+ batch_i) > 0:
+                model.save_weights("%s/YOLO3_%d.weights" % (args['save_folder'], epoch*len(dataloader)+batch_i))
+                print("Weights saved for epoch {}/{}, batch {}/{}".format(epoch, iters, batch_i, len(dataloader)))
+
+            if args['visdom']:
+                # viz.update_vis_plot(iteration_total, loss_l.data.item(), loss_c.data.item(), iter_plot, epoch_plot, 'append')
+                viz.update_vis_plot(iteration_total, loss.item(), 0, iter_plot, None, 'append')
+            iteration_total += 1
 
 if __name__ == "__main__":
     args = arg_parser()
