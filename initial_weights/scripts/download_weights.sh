87ba892cce008239ca866aa56cf85cf518a4545f
Valentin Py <valentin.py@he-arc.ch>
2019-01-09 14:54:45 +0100

    add script to download weights

--- initial_weights/scripts/download_weights.sh
+++ initial_weights/scripts/download_weights.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget https://pjreddie.com/media/files/yolov3.weights
+wget -O mobilenet2.pth https://storage.googleapis.com/models-hao/mb2-imagenet-71_8.pth mobilenet2.pth
+wget https://s3.amazonaws.com/amdegroot-models/vgg16_reducedfc.pth
