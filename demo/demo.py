98926824802bbfa25514a054c43341a5be11cf84
Valentin Py <valentin.py@he-arc.ch>
2019-01-10 14:15:42 +0100

    cleaning, demo compatible with mobilenet2

--- demo/demo.py
+++ demo/demo.py
@@ -3,13 +3,17 @@ import os
 import glob
 import sys
 import cv2
+import signal
+from collections import OrderedDict
+
 from utils.timer import Timer
 from utils.str2bool import str2bool
 from config.parse_config import *
 from config.load_classes import load_classes
-from models.vgg16_ssd import build_ssd
 from models.yolo3 import *
 from models.yolo3_utils import *
+from models.vgg16_ssd import build_vgg_ssd
+from models.mobilenet2_ssd import build_mobilenet_ssd
 
 
 import torch
@@ -25,11 +29,28 @@ def arg_parser():
     parser.add_argument('--image_fusion', default=-1, type=int, help='[KAIST]: type of image fusion: [0: visible], [1: lwir] [2: inverted LWIR] [...]')  # TODO VPY update when required
     parser.add_argument('--corrected_annotations', default=False, type=str2bool, help='[KAIST] do we use the corrected annotations ? (must ahve compatible imageset (VPY-test-strict-type-5)')
     parser.add_argument("--data_config_path", type=str, default=None, help="path to data config file")
-    parser.add_argument("--model", type=str, default=None, help="Model to train, either 'SSD' or 'YOLO'")
+    parser.add_argument("--model", type=str, default=None, help="Model to demo, either 'VGG_SSD','MOBILENET2_SSD' or 'YOLO'")
 
-    args = vars(parser.parse_args())
-    config = parse_data_config(args['data_config_path'])
-    args = {**args, **config}
+    args_cmd = vars(parser.parse_args())
+    args_cmd = {key: value for key, value in args_cmd.items() if value is not None}  # remove non attributed values from parser
+
+    args_file = parse_data_config(args_cmd['data_config_path'])
+    if args_cmd['model'] == "VGG_SSD":
+        args_file = {key.replace("vgg_", ""): value for key, value in args_file.items() if
+                     (not key.startswith("mobilenet")) and (not key.startswith("yolo"))}
+    elif args_cmd['model'] == "MOBILENET2_SSD":
+        args_file = {key.replace("mobilenet_", ""): value for key, value in args_file.items() if
+                     (not key.startswith("vgg")) and (not key.startswith("yolo"))}
+    elif args_cmd["model"] == "YOLO":
+        args_file = {key: value for key, value in args_file.items() if (not key.startswith("vgg")) and (not key.startswith("mobilenet"))}
+
+    # remove duplicates entries, command line has priority on config file
+    duplicates = (args_file.keys() & args_cmd.keys())
+    for key in duplicates:
+        del args_file[key]
+
+    args = {**args_cmd, **args_file}
+
     return args
 
 def test_net(model_name, net, cuda, dataset, conf_thres, nms_thres, classes, transform):
@@ -57,7 +78,7 @@ def test_net(model_name, net, cuda, dataset, conf_thres, nms_thres, classes, tra
         if cuda:
             input_imgs = input_imgs.cuda()
 
-        if model_name == "SSD":
+        if model_name in {"VGG_SSD", "MOBILENET2_SSD"}:
             y = net(input_imgs)
             detections = y.data
 
@@ -88,7 +109,7 @@ def test_net(model_name, net, cuda, dataset, conf_thres, nms_thres, classes, tra
         # --------------------------
         # Detections
         # --------------------------
-        if model_name == "SSD":
+        if model_name in {"VGG_SSD", "MOBILENET2_SSD"}:
             scale = torch.Tensor([img_gt.shape[1], img_gt.shape[0], img_gt.shape[1], img_gt.shape[0]])
             pred_num = 0
             for i in range(detections.size(1)):  # loop for all classes
@@ -140,7 +161,6 @@ def test_net(model_name, net, cuda, dataset, conf_thres, nms_thres, classes, tra
 def main(args):
     model_name = args["model"]
     dataset_name = args["name"]
-    imageset_name = args["image_set"].split("/")[-1].split(".")[0]
     cuda = torch.cuda.is_available() and args['cuda']
 
     if cuda:
@@ -152,21 +172,39 @@ def main(args):
     num_classes = len(classes) + 1  # +1 for background
 
     # load net
-    if model_name == "SSD":
-        net = build_ssd(phase='test', size=300, num_classes=num_classes, dataset=dataset_name, cfg=args) # initialize SSD
+    print("Loading weights from file: {}".format(args["trained_model"]))
+    if model_name == "VGG_SSD":
+        net = build_vgg_ssd(phase='test', size=300, num_classes=num_classes,cfg=args) # initialize SSD
+        try:
+            net.load_state_dict(torch.load(args['trained_model']))
+        except:
+            # If we try to load a model with "vgg" conv instead of "basenet" conv, as the name changed to be generic and we don't want to train again,
+            # we just have to change names in the OrderDict loaded form file
+            old_model = torch.load(args['trained_model'])
+            new_model = OrderedDict()
+            for key, value in old_model.items():
+                new_model[key.replace("vgg", "basenet")] = value
+            new_model._metadata = OrderedDict()
+            for key, value in old_model._metadata.items():
+                new_model._metadata[key.replace("vgg", "basenet")] = value
+            net.load_state_dict(new_model)
+
+    elif model_name == "MOBILENET2_SSD":
+        net = build_mobilenet_ssd(phase='test', size=320, num_classes=num_classes, cfg=args)  # initialize SSD
         net.load_state_dict(torch.load(args['trained_model']))
+
     elif model_name == "YOLO":
         net = Darknet(args['yolo_model_config_path'], img_size=args['yolo_img_size'])
         net.load_weights(args['trained_model'])
 
-    net.eval()
-
     print('Finished loading model!')
+
+    net.eval()
 
     # get augmentation function
     if model_name == "YOLO":
         from augmentations.YOLOaugmentations import YOLOaugmentation
-    elif model_name == "SSD":
+    elif model_name in {"MOBILENET2_SSD", "VGG_SSD"}:
         from augmentations.SSDaugmentations import SSDAugmentationDemo
 
     # load data
@@ -176,7 +214,7 @@ def main(args):
         dataset = VOCDetection(root=args['dataset_root'], image_sets=[('2007', 'test')], transform=None, target_transform=VOCAnnotationTransform())
     elif dataset_name == 'KAIST':
         from data.kaist import KAISTDetection, KAISTAnnotationTransform
-        transform_fct = YOLOaugmentation(args['yolo_img_size']) if model_name == "YOLO" else SSDAugmentationDemo(300)
+        transform_fct = YOLOaugmentation(args['yolo_img_size']) if model_name == "YOLO" else SSDAugmentationDemo(args["ssd_min_dim"])
         dataset = KAISTDetection(root=args['dataset_root'], image_set=args['image_set'], transform=transform_fct,
                                  image_fusion=args['image_fusion'], corrected_annotations=args['corrected_annotations'],
                                  target_transform=KAISTAnnotationTransform(output_format="VOC_EVAL"))
@@ -196,16 +234,12 @@ def main(args):
         nms_thres = args['yolo_nms_thres']
         transform_fct = BaseTransform(args["yolo_img_size"], (104, 117, 123))#None
 
-    elif model_name == "SSD":
+    elif model_name in {"VGG_SSD", "MOBILENET2_SSD"}:
         conf_thresh = args['ssd_visual_threshold']
         nms_thres = None
-        transform_fct = BaseTransform(net.size, (104, 117, 123))
+        transform_fct = BaseTransform(args["ssd_min_dim"], (104, 117, 123))
 
     test_net(model_name=model_name, net=net, cuda=cuda, dataset=dataset, conf_thres=conf_thresh, nms_thres=nms_thres, classes=classes, transform=transform_fct)
-    # test_net(net=net, cuda=args['cuda'], testset=dataset, transform=BaseTransform(net.size, (104, 117, 123)), thresh=args['ssd_visual_threshold'], labelmap=classes) #TODO VPY: MEAN ?!
-    # test_net(net=net, dataloader=dataloader, img_size=args['yolo_img_size'], classes=classes, conf_thres=args['yolo_conf_thres'], nms_thres=args['yolo_nms_thres'])#, testset=testset):#, transform=BaseTransform(net.size, (104, 117, 123)), thresh=args['ssd_visual_threshold'], labelmap=labelmap)  # TODO VPY: MEAN ?!
-
-
 
 
 def check_args(args):
@@ -219,12 +253,12 @@ def check_args(args):
         print('Must specify *existing* dataset_root')
         sys.exit(-1)
 
-    if args["model"].upper() == "SSD":
+    if args["model"] in {"VGG_SSD", "MOBILENET2_SSD"}:
         if args["name"] not in {"VOC", "KAIST"}:
             print("Dataset {} not supported with model {}".format(args["name"], args["model"]))
             sys.exit(-1)
 
-    elif args["model"].upper() == "YOLO":
+    elif args["model"] == "YOLO":
         if args["name"] not in {"COCO", "KAIST"}:
             print("Dataset {} not supported with model {}".format(args["name"], args["model"]))
             sys.exit(-1)
@@ -232,7 +266,12 @@ def check_args(args):
         print("Model {} is not supported".format(args["model"]))
         sys.exit(-1)
 
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     args = arg_parser()
     check_args(args)
     main(args)
